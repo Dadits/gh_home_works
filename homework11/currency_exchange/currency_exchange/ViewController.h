8e8127453bf3e3f28d9ecf89b7b29bb2d4ef54b8
Anton <maks2109@gmail.com>
2015-01-25 15:59:00 +0200

    made lots of modifications

--- homework11/currency_exchange/currency_exchange/ViewController.h
+++ homework11/currency_exchange/currency_exchange/ViewController.h
@@ -10,33 +10,5 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *rULabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *eULabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *usUaLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *uRLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *uELabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *uaUsLabel;
-
-@property (weak, nonatomic) IBOutlet UIPickerView *currencyTypePicker;
-
-@property (weak, nonatomic) IBOutlet UITextField *currencyInput;
-
-
-- (IBAction)inputTextchanged:(id)sender;
-
-@property (weak, nonatomic) IBOutlet UIButton *convertButton;
-
-
-- (IBAction)convertComplite:(id)sender;
-
-@property (strong, nonatomic) IBOutlet UITextField *resultText;
-
-@property NSArray *PickerDatasource;
 
 @end
-
--- homework11/currency_exchange/currency_exchange/ViewController.m
+++ homework11/currency_exchange/currency_exchange/ViewController.m
@@ -7,155 +7,116 @@
 //
 
 #import "ViewController.h"
+#import "Currency.h"
 
-@interface ViewController ()<NSURLConnectionDataDelegate,UIPickerViewDataSource,UIPickerViewDelegate>
+@interface ViewController ()<NSURLConnectionDataDelegate, UIPickerViewDelegate, UIPickerViewDataSource>
+{
+    NSMutableArray *currencies;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *sum;
+@property (weak, nonatomic) IBOutlet UILabel *resultField;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickFrom;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickTo;
+
+
+@property (nonatomic, strong) NSURLConnection *connection;
 
 @end
 
+
 @implementation ViewController
-
-@synthesize rULabel;
-
-@synthesize eULabel;
-
-@synthesize usUaLabel;
-
-@synthesize uRLabel;
-
-@synthesize uELabel;
-
-@synthesize uaUsLabel;
-
-@synthesize currencyTypePicker;
-
-@synthesize PickerDatasource;
-
-@synthesize currencyInput;
-
-@synthesize convertButton;
-
-@synthesize resultText;
-
-int leftRow = 0;
-int leftComponent =0;
-int rightRow = 0;
-int rightComponent = 1;
-
-NSArray *arrayCarrencys;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    self.PickerDatasource = [NSArray arrayWithObjects:@"UAH", @"RUB", @"EUR", @"USD", nil];
-    
-    NSURL *url = [NSURL URLWithString:@"https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"];
+    currencies = [NSMutableArray array];
+    [currencies addObject:[self hryvna]];
+    [self getCurrencys];
+}
+
+- (void)getCurrencys{
+    NSString *urlString = @"https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5";
+    NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
-    [connection start];
+    _connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
 }
 
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
-    arrayCarrencys = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-    rULabel.text = arrayCarrencys[0][@"buy"];
-    eULabel.text = arrayCarrencys[1][@"buy"];
-    usUaLabel.text = arrayCarrencys[2][@"buy"];
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
     
-    uRLabel.text = arrayCarrencys[0][@"sale"];
-    uELabel.text = arrayCarrencys[1][@"sale"];
-    uaUsLabel.text = arrayCarrencys[2][@"sale"];
-}
-
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    return 2;
-}
-
--(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-    return [PickerDatasource count];
-}
-
--(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return [PickerDatasource objectAtIndex:row];
-}
-
--(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
-    if (component == 0){
-        leftRow = (int)row;
-        leftComponent = 0;
+    if (!dictionary) {
+        //todo set status with error
+        return;
     }
-    if (component == 1) {
-        rightComponent = 1;
-        rightRow = (int)row;
-    }
+    NSLog(@"%@", dictionary);
     
+    for (NSDictionary *current in dictionary) {
+        Currency *currency = [[Currency new] initWithDictionary:current];
+        [currencies addObject:currency];
+        
+        NSLog(@"%@", currency.codeName);
+        
+        //        [currency saveToUserDefaults];
+        
+    }
+    [_pickFrom reloadAllComponents];
+    [_pickTo reloadAllComponents];
+}
+
+
+- (IBAction)doConvert:(UIButton *)sender {
+    //show result
+    //if same rows - send 1
+    NSInteger selectedRowToSell = [_pickFrom selectedRowInComponent:0];
+    NSInteger selectedRowToBuy = [_pickTo selectedRowInComponent:0];
+    
+    if (selectedRowToSell == selectedRowToBuy) {
+        [self makeConvert:1 to:1];
+    } else {
+        [self makeConvert:[[currencies[selectedRowToSell] sale] floatValue] to:[[currencies[selectedRowToBuy] buy] floatValue]];
+    }
+}
+
+
+-(void)makeConvert:(float)sale to:(float)buy {
+    float enteretInt = [_sum.text floatValue];
+    float result = sale * enteretInt / buy;
+    _resultField.text = [NSString stringWithFormat:@"%.02f", result];
     
 }
+
+-(Currency*)hryvna
+{
+    Currency *uah = [Currency new];
+    uah.codeName = @"UAH";
+    uah.codeNational = @"UAH";
+    uah.sale = @"1";
+    uah.buy = @"1";
+    return uah;
+}
+
+
+#pragma mark- UIPickerView Delegate & DataSource
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return currencies.count;
+}
+
+- (NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:[currencies[row] codeName] attributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:((float)103/255.0) green:((float)167/255.0) blue:((float)52/255.0) alpha:1.0]}];
+    
+    return attString;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)inputTextchanged:(id)sender {
-    [currencyInput addTarget:self action:@selector(textFieldShouldEndEditing:) forControlEvents:UIControlEventEditingDidEnd];
-}
-
-- (IBAction)convertComplite:(id)sender {
-    
-    //если валюта совпадает
-    if (leftRow == rightRow) {resultText.text = currencyInput.text;}
-    
-    //обрабатываем покупку валют за гривну
-    if (leftRow == 0 & rightRow == 1) {
-        NSString *stringUahToRub = arrayCarrencys[0][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue/stringUahToRub.floatValue];}
-    if (leftRow == 0 & rightRow == 2) {
-        NSString *stringUahToEur = arrayCarrencys[1][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue/stringUahToEur.floatValue];}
-    if (leftRow == 0 & rightRow == 3) {
-        NSString *stringUahToUsd = arrayCarrencys[2][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue/stringUahToUsd.floatValue];}
-    
-    //обрабатываем покупку валют за рубли
-    if (leftRow == 1 & rightRow == 0) {
-        NSString *stringRubToUah = arrayCarrencys[0][@"buy"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringRubToUah.floatValue];}
-    if (leftRow == 1 & rightRow == 2) {
-        NSString *stringRubToUAH = arrayCarrencys[0][@"buy"];
-        NSString *stringUahToEur = arrayCarrencys[1][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringRubToUAH.floatValue/stringUahToEur.floatValue];}
-    if (leftRow == 1 & rightRow == 3) {
-        NSString *stringRubToUAH = arrayCarrencys[0][@"buy"];
-        NSString *stringUahToUSD = arrayCarrencys[2][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringRubToUAH.floatValue/stringUahToUSD.floatValue];}
-    
-    //обрабатываем покупку вылют за евро
-    if (leftRow == 2 & rightRow == 0) {
-        NSString *stringEurToUah = arrayCarrencys[1][@"buy"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringEurToUah.floatValue];}
-    if (leftRow == 2 & rightRow == 1) {
-        NSString *stringEurToUah = arrayCarrencys[1][@"buy"];
-        NSString *stringUahToRub = arrayCarrencys[0][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringEurToUah.floatValue/stringUahToRub.floatValue];}
-    if (leftRow == 2 & rightRow == 3) {
-        NSString *stringEurToUah = arrayCarrencys[1][@"buy"];
-        NSString *stringUahToUsd = arrayCarrencys[2][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringEurToUah.floatValue/stringUahToUsd.floatValue];}
-    
-    //обрабатываем покупку валют за доллар
-    if (leftRow == 3 & rightRow == 0) {
-        NSString *stringUsdToUah = arrayCarrencys[2][@"buy"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringUsdToUah.floatValue];}
-    if (leftRow == 3 & rightRow == 1) {
-        NSString *stringUsdToUah = arrayCarrencys[2][@"buy"];
-        NSString *stringUahToRub = arrayCarrencys[0][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringUsdToUah.floatValue/stringUahToRub.floatValue];}
-    if (leftRow == 3 & rightRow == 2) {
-        NSString *stringUsdToUah = arrayCarrencys[2][@"buy"];
-        NSString *stringUahToEur = arrayCarrencys[1][@"sale"];
-        resultText.text = [NSString stringWithFormat:@"%0.2f", currencyInput.text.integerValue*stringUsdToUah.floatValue/stringUahToEur.floatValue];}
-    
 }
 
 @end
